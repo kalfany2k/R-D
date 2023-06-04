3c5066de5fe5681e2232265da6408e96b11dd5a0
kalfany2k <darian_1973@yahoo.com>
2023-06-04 23:19:06 +0200

    Nu stiu ce am schimbat sper ca e ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import { useState } from "react";
 import NavBar from "./Components/NavBar";
 import SearchBar from "./Components/SearchBar";
-import HowItWorks from "./Components/HowItWorks";
-import BecomeAPartner from "./Components/BecomeaPartner"; // Don't forget to create this component
-import EventCategoryList from "./Components/EventCategoryList";
+import EventGrid from "../src/Components/EventGrid";
+import axios from "axios";
 
 function App() {
   const [isMenuActive, setMenuActive] = useState(false);
@@ -14,11 +13,9 @@ function App() {
       <Grid
         templateAreas={{
           base: '"nav" "main"',
-          lg: '"nav nav" "aside main"',
         }}
         templateColumns={{
           base: "1fr",
-          lg: "400px 1fr",
         }}
       >
         <GridItem area="nav" className="nav-search">
@@ -29,12 +26,9 @@ function App() {
           <SearchBar />
         </GridItem>
 
-        <Show above="lg">
-          <GridItem area="aside">
-            <EventCategoryList />
-          </GridItem>
-        </Show>
-        <GridItem area="main">"Main"</GridItem>
+        <GridItem area="main">
+          <EventGrid />
+        </GridItem>
       </Grid>
     </div>
   );
